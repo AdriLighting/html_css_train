004034f88f1d3f78b57bfd5d9ea76d2f410f9945
Adrien Grellard <grellard.adrien@gmail.com>
2023-04-19 08:25:38 +0200

    update

--- js/script.js
+++ js/script.js
@@ -2,12 +2,13 @@
 document.getElementById("link_1").addEventListener("click", ()=>console.log("link_1"))
 document.getElementById("link_2").addEventListener("click", ()=>console.log("link_2"))
 
-let array = document.querySelectorAll("button")
-array.forEach(element => {
+let array = document.querySelectorAll("div")
+array.forEach((element, index)  => {
     var sClass = element.getAttribute("class");
     if (sClass == "filter-boxe") {
+        console.log(index, element);
         element.addEventListener("click", () => {
-            console.log(element);
+            console.log(element.children[1].innerText);
         })
         
     }
